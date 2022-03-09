01704efa4ab5d91014d42130098125e7ba707144
heroldg <80465743+heroldg@users.noreply.github.com>
2022-03-09 13:42:54 +0100

    background effect

--- webcv.js
+++ webcv.js
@@ -1,26 +1,27 @@
-/* algo backgroundColor home page*/
+/*   changement de backgroundColor home page*/
 
 var body = document.querySelector("body")
 var nav = document.querySelector("#navbar")
 
 
 nav.onmouseover = function () {
-    body.classList.add("bodyblack")
-    nav.style.backgroundColor = "black"
+    body.classList.add("bodyblack");
+    nav.style.backgroundColor = "black";
 }
 
 nav.onmouseout = function () {
     body.classList.remove("bodyblack")
-    nav.style.backgroundColor = "rgb(206, 198, 182)"
+    nav.style.backgroundColor = "rgb(206, 198, 182)";
+    nav.style.color ="black";
 }
 
-/* algo pour changer forme de la souris (cursor)*/
+/*  changer forme de la souris (cursor)*/
 
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
     cursor.setAttribute('style', 'top:'+(e.pageY - 35)+"px; left:"+(e.pageX - 35)+"px;")
-})
+});
 
 document.addEventListener('click', ()=>{
     cursor.classList.add('expand');
@@ -28,16 +29,17 @@ document.addEventListener('click', ()=>{
     setTimeout(()=>{
         cursor.classList.remove("expand");
     }, 500);
-})
+});
 
-function scrollAbout () {
-    window.scrollBy(-10,0)   
-}
-
-function scrollWork () {
-    window.scrollBy(-10,0)
-}
+/* Scroll latéral de la nav about, work , contact pour le format au dessus de 800px */
 
 function scrollContact () {
     window.scrollBy(10,0)
-}
+};
+function scrollAbout () {
+    window.scrollBy(-10,0)   
+};
+
+function scrollWork () {
+    window.scrollBy(-10,0)
+};
